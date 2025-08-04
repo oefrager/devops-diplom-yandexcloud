a83d03902cde6d9479f9d431a55d2d184166c1a1
goi <goi@belkam.com>
2025-08-04 17:20:54 +0400

    изменено:      kubernetes/prometheus.tf

--- kubernetes/prometheus.tf
+++ kubernetes/prometheus.tf
@@ -8,19 +8,22 @@ resource "null_resource" "deploy_grafana" {
       # Установка Helm
       "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash",
     
-      # Установка prometheus-operator deployment
-    #  "kubectl create ns monitoring",
-      "git clone https://github.com/coreos/prometheus-operator.git",
-    
-      "cd /home/ubuntu/prometheus-operator",
-      "helm install  --set rbacEnable=true helm/prometheus-operator",
+      #Клонируем  kube-prometheus репозиторий
+      "git clone https://github.com/prometheus-operator/kube-prometheus.git",
+      "cd kube-prometheus",
+
+      #Deploy Prometheus Monitoring Stack on Kubernetes
+      "kubectl create -f manifests/setup",
+      "kubectl create -f manifests/",
+
+      #"kubectl --namespace monitoring get networkpolicies,"
+      #"kubectl -n monitoring delete networkpolicies.networking.k8s.io --all,"
       
-      # Установка Prometheus, Alertmanager specs, Grafana deployment, kube-prometheus
-      "helm install --name prometheus --set serviceMonitorsSelector.app=prometheus --set ruleSelector.app=prometheus helm/prometheus",
-      "helm install --name alertmanager helm/alertmanager",
-      "helm install --name grafana helm/grafana",
-      "helm install --name kube-prometheus helm/kube-prometheus"
-    ]  
+      # Доступ к Prometheus, Alertmanager, Grafana используя NodePort
+      "kubectl --namespace monitoring patch svc prometheus-k8s -p '{\"spec\": {\"type\": \"NodePort\"}}'",
+      "kubectl --namespace monitoring patch svc alertmanager-main -p '{\"spec\": {\"type\": \"NodePort\"}}'",
+      "kubectl --namespace monitoring patch svc grafana -p '{\"spec\": {\"type\": \"NodePort\"}}'"
+    ]
     connection {
       type        = "ssh"
       user        = "ubuntu"
